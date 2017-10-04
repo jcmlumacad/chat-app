16c1c88797584ecb05c92d319c7d3f43ff02e5b0
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-10-04 12:16:27 +0800

    test

--- app.js
+++ app.js
@@ -12,34 +12,35 @@ import './lib/helpers'
 
 let app = express()
 
-// Gzip Compress for compressing all accessed files
-compress(app)
-
-// Libraries for client-side
-app.use('/bower_components', express.static(`${__dirname}/bower_components`))
-// Assets: JS & CSS
-app.use('/assets', express.static(`${__dirname}/public/assets`))
-// HTML Files
-app.use('/views', express.static(`${__dirname}/resources/views`))
-
-// This library uses req.body for requests like POST method, etc.
-bodyParser(app)
-// This library uses for security purposes like session, crsf, passport, and helmet.
-middleware(app)
-// Set csrf token in cookie
-csrf(app)
-// Centralized error handler
-handler(app)
-// Database configuration
-mongoose()
-// Database Seeder
-databaseSeeder()
-// Set of routes in an application
-// routes(app)
 app.get('/', (req, res) => {
     res.json(true)
 })
-// Passport configuration
-passport()
+
+// // Gzip Compress for compressing all accessed files
+// compress(app)
+
+// // Libraries for client-side
+// app.use('/bower_components', express.static(`${__dirname}/bower_components`))
+// // Assets: JS & CSS
+// app.use('/assets', express.static(`${__dirname}/public/assets`))
+// // HTML Files
+// app.use('/views', express.static(`${__dirname}/resources/views`))
+
+// // This library uses req.body for requests like POST method, etc.
+// bodyParser(app)
+// // This library uses for security purposes like session, crsf, passport, and helmet.
+// middleware(app)
+// // Set csrf token in cookie
+// csrf(app)
+// // Centralized error handler
+// handler(app)
+// // Database configuration
+// mongoose()
+// // Database Seeder
+// databaseSeeder()
+// // Set of routes in an application
+// // routes(app)
+// // Passport configuration
+// passport()
 
 export default app
